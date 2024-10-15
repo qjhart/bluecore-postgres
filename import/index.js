a678fb5ce87082cef16419a2726d257500dd8d18
Quinn Hart <qjhart@ucdavis.edu>
2024-10-14 18:01:13 -0700

    Inserting iri records

--- import/index.js
+++ import/index.js
@@ -1,7 +1,6 @@
 import { Command } from 'commander';
 import pg from 'pg';
 const { Client } = pg;
-//import { Client } from 'pg';
 import ParserJsonld from '@rdfjs/parser-jsonld';
 import { Readable } from 'stream';
 import fs from 'fs';
@@ -12,11 +11,19 @@ import JsonLdProcessor from 'jsonld';
 const jp = new JsonLdProcessor();
 import md5 from 'md5';
 
-// Create an RDF dataset to act as an in-memory RDF sink
-const dataset = rdf.dataset();
+// Context and frame info
+import context from '../schema/context.json' assert { type: 'json' };
+import instance_frame from '../schema/frames/instance.json' assert { type: 'json' };
+
+instance_frame['@context'] = context["@context"];
+
+const schema = {};
+schema.context = context;
+schema.frame = {};
+schema.frame.default = instance_frame;
 
 // Function to read JSON-LD from a file and populate the dataset
-async function loadJsonldToDataset(filePath) {
+async function loadJsonldToDataset(dataset,filePath) {
     return new Promise((resolve, reject) => {
         // Read the JSON-LD file
         const jsonldContent = fs.readFileSync(filePath, 'utf8');
@@ -42,7 +49,6 @@ async function loadJsonldToDataset(filePath) {
 
         // Resolve the promise when parsing is finished
         output.on('end', () => {
-            console.log('Parsing finished!');
             resolve(); // Resolve the promise
         });
 
@@ -74,20 +80,21 @@ async function main() {
     console.log('Query result:', result.rows);
   } catch (err) {
     console.error('Error connecting to the database:', err);
-  } finally {
-    // Ensure the client is closed after the query
-    await client.end();
-    console.log('Disconnected from the database.');
   }
 
   // get each file from command line options
   for (let i = 0; i < program.args.length; i++) {
+    // Create an RDF dataset to act as an in-memory RDF sink
+    const dataset = rdf.dataset();
+
     const file = program.args[i];
+    console.log('Processing file:', file);
     const namedNode = {};
     const partOf = {};
     const blankNode = {};
+    // console.log('blankNode:', blankNode);
     try {
-      await loadJsonldToDataset(file); // Wait for the dataset to be populated
+      await loadJsonldToDataset(dataset,file); // Wait for the dataset to be populated
       const allQuads = dataset.match(null, null, null);
       dataset.forEach(quad => {
         if (quad.subject.termType === 'NamedNode') {
@@ -108,13 +115,25 @@ async function main() {
             namedNode[partOf[quad.subject.value]].add(quad);
           }
         } else {
-          console.log('Unknown:', quad.subject.value);
+          console.error('Unknown:', quad.subject.value);
         }
         // Now add blank nodes to their parent named node.
-        if (quad.object.termType === 'BlankNode') {
+        if (quad.object.termType === 'BlankNode' &&
+            quad.subject.value !== quad.object.value) {
+//          if (quad.object.value === "b656iddOtlocdOtgovresourcesworks11199947" ||
+//              quad.object.value === "b650iddOtlocdOtgovresourcesworks11199947") {
+//            console.log('Found it:', quad);
+//            console.log('partOf:', partOf[quad.subject.value]);
+//          }
           //console.log(`${quad.object.value} is part of ${partOf[quad.subject.value]}`);
           //console.log(quad);
           partOf[quad.object.value] = partOf[quad.subject.value];
+          // for any other part of with this object.value change to subject.value
+          for (const [key, value] of Object.entries(partOf)) {
+            if (value === quad.object.value) {
+              partOf[key] = partOf[quad.subject.value];
+            }
+          }
         }
 //        console.log(quad.subject.value, quad.predicate.value, quad.object.value);
       });
@@ -125,54 +144,72 @@ async function main() {
     // For each named node, print the triples
     for (const key in blankNode) {
       if (namedNode[partOf[key]] === undefined) {
+        console.error('Error: Blank node not part of a named node:', key);
+      console.log('Blank Node:', key);
+      console.log(`partOf: ${partOf[key]}`);
+        console.log(Object.keys(namedNode));
+//        continue;
         throw new Error('No parent for blank node: ' + key);
       }
-      console.log(`Adding blank node ${key} to ${partOf[key]}`);
+//      console.log(`Adding blank node ${key} to ${partOf[key]}`);
       blankNode[key].forEach(quad => {
         namedNode[partOf[key]].add(quad);
       });
     }
     for (const key in namedNode) {
-      console.log('Named node:', key);
+//      console.log('Named node:', key);
+      let label = '';
       const needNode = {};
       const types = {};
       const quads = namedNode[key].match(null, null, null);
       quads.forEach(quad => {
-        if (quad.subject.termType === 'BlankNode') {
+        if (quad.subject.termType === 'BlankNode' &&
+            ! quad.subject.value.startsWith('_:')) {
           quad.subject.value = `_:${quad.subject.value}`;
         }
-        if (quad.object.termType === 'BlankNode') {
+        if (quad.object.termType === 'BlankNode' &&
+            ! quad.object.value.startsWith('_:')) {
           quad.object.value = `_:${quad.object.value}`;
         } else if (quad.object.termType === 'NamedNode') {
           needNode[quad.object.value] = true;
           if (quad.subject.value === key && quad.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
             types[quad.object.value] = true;
           }
-          // for now don't include types as needNode
-          if (quad.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
-            delete needNode[quad.object.value];
-          }
+        }
+        if (quad.subject.value === key &&
+            quad.predicate.value === 'http://www.w3.org/2000/01/rdf-schema#label') {
+          // for now, one label per node
+          label = quad.object.value;
+        }
+        // for now don't include types as needNode
+        if (quad.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
+          delete needNode[quad.object.value];
         }
       });
       const jsonld = await jp.fromRDF(quads);
       const canonized = await jp.canonize(jsonld, {format: 'application/n-quads'});
-      const framed = await jp.frame(jsonld, {
-        '@context': {
-          '@vocab': 'http://schema.org/',
-          'type': '@type'
-        },
-        '@type': Object.keys(types)
-      });
+      const framed = await jp.frame(jsonld,
+                                    { ...schema.frame.default,
+                                      '@type': Object.keys(types) });
       const chk = md5(canonized);
-      console.log(`${key} ${chk}`);
-      console.log("\t",Object.keys(types));
-      console.log("\t",Object.keys(needNode));
-      console.log(JSON.stringify(jsonld, null, 2));
-//      namedNode[key].forEach(quad => {
-//        console.log(key,quad.subject.value, quad.predicate.value, quad.object.value);
-//      });
+//      console.log(`${key} ${chk}`);
+//      console.log("\t",Object.keys(types));
+//      console.log("\t",Object.keys(needNode));
+      delete framed['@context'];
+//      console.log(JSON.stringify(framed, null, 2));
+      // Insert the JSON-LD into the database
+      const insertQuery = `INSERT INTO iri (iri, chk, label, jsonld) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING`;
+      const insertValues = [key, chk, label, JSON.stringify(framed)];
+      try {
+        await client.query(insertQuery, insertValues);
+      } catch (err) {
+        console.error('Error inserting JSON-LD:', err);
+      }
     }
   }
+  // Disconnect from the database
+  await client.end();
+
 }
 
 const program = new Command();
